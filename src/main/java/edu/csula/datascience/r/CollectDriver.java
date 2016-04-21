088d06f824a477721910e02cb6a9fda8ee4e9b8e
Tony Guardado <guardado.anthonyj@gmail.com>
2016-04-21 13:45:30 -0700

    add main driver

--- src/main/java/edu/csula/datascience/r/CollectDriver.java
+++ src/main/java/edu/csula/datascience/r/CollectDriver.java
@@ -1,7 +1,13 @@
 package edu.csula.datascience.r;
 
+import edu.csula.datascience.r.auth.RedditOAuth;
+import net.dean.jraw.RedditClient;
+
 /**
  * Created by tj on 4/21/16.
  */
 public class CollectDriver {
+  public static void main(String[] args) throws Exception{
+    RedditOAuth auth = new RedditOAuth();
+  }
 }
