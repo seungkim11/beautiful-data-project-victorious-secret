a5550c5002490bf8a786e22620909f97ed2790d7
Tony Guardado <guardado.anthonyj@gmail.com>
2016-04-21 13:45:44 -0700

    add authentication check

--- src/main/java/edu/csula/datascience/r/acquisition/RedditSource.java
+++ src/main/java/edu/csula/datascience/r/acquisition/RedditSource.java
@@ -1,6 +1,9 @@
 package edu.csula.datascience.r.acquisition;
 
 import edu.csula.datascience.acquisition.Source;
+import edu.csula.datascience.r.auth.RedditOAuth;
+import net.dean.jraw.RedditClient;
+import net.dean.jraw.http.UserAgent;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -21,23 +24,37 @@ public class RedditSource implements Source<Post> {
     showerthoughts, explainlikeimfive, askreddit, jokes, askscience, an
      */
     // FIXME: eventually, we should get a list containeing what subreddits to use as source
-    subreddits = new Arrays.asList("askreddit");
+    this(new Arrays.asList("askreddit"));
+  }
+
+  public RedditSource(List<String> subreddits){
+    this.subreddits = subreddits;
   }
 
   public boolean hasNext(){
-
+    // FIXME
+    return false;
   }
 
   public Collection<Post> next(){
+    // FIXME
     return null;
   }
 
   public void remove(){
-
+    // FIXME
   }
 
   private List<Post> getPosts(){
     // posts for which subreddit?
     List<Post> posts = new ArrayList<>();
+    UserAgent myUserAgent = UserAgent.of("desktop", "awesomescript", "v0.1", "victorious-secret");
+    RedditClient redditClient = new RedditClient(myUserAgent);
+    if(!redditClient.isAuthenticated()){
+      RedditOAuth auth = new RedditOAuth();
+      auth.authenticate(redditClient);
+    }
+
+
   }
 }
