27ea6740bae1b541045d0fc7759bad33a7a755c2
Tony Guardado <guardado.anthonyj@gmail.com>
2016-04-21 13:36:09 -0700

    add constructor

--- src/main/java/edu/csula/datascience/r/acquisition/RedditSource.java
+++ src/main/java/edu/csula/datascience/r/acquisition/RedditSource.java
@@ -2,19 +2,42 @@ package edu.csula.datascience.r.acquisition;
 
 import edu.csula.datascience.acquisition.Source;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by tj on 4/21/16.
  */
 public class RedditSource implements Source<Post> {
+
+  private final List<String> subreddits;
+
+  public RedditSource(){
+    /*
+    TODO:
+    perhaps each source should correspond to a subreddit? or subset of subreddit
+    funny, news, gifs, reddit pics, etc]
+    showerthoughts, explainlikeimfive, askreddit, jokes, askscience, an
+     */
+    // FIXME: eventually, we should get a list containeing what subreddits to use as source
+    subreddits = new Arrays.asList("askreddit");
+  }
+
   public boolean hasNext(){
 
   }
 
   public Collection<Post> next(){
-
+    return null;
   }
 
   public void remove(){
 
+  }
+
+  private List<Post> getPosts(){
+    // posts for which subreddit?
+    List<Post> posts = new ArrayList<>();
   }
 }
