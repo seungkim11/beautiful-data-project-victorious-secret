84c1a7af852e201baefa9ac575fb8c716129e64e
Tony Guardado <guardado.anthonyj@gmail.com>
2016-04-21 13:25:03 -0700

    add source class

---
+++ src/main/java/edu/csula/datascience/r/acquisition/RedditSource.java
@@ -0,0 +1,20 @@
+package edu.csula.datascience.r.acquisition;
+
+import edu.csula.datascience.acquisition.Source;
+
+/**
+ * Created by tj on 4/21/16.
+ */
+public class RedditSource implements Source<Post> {
+  public boolean hasNext(){
+
+  }
+
+  public Collection<Post> next(){
+
+  }
+
+  public void remove(){
+
+  }
+}
