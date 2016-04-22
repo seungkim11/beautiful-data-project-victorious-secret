f1a0d16e6ec6288680eadb160ec089bb9700f68c
Tony Guardado <guardado.anthonyj@gmail.com>
2016-04-22 13:00:52 -0700

    add database connection for collector save method

--- src/main/java/edu/csula/datascience/r/acquisition/SubmissionCollector.java
+++ src/main/java/edu/csula/datascience/r/acquisition/SubmissionCollector.java
@@ -1,19 +1,31 @@
 package edu.csula.datascience.r.acquisition;
 
+import com.google.appengine.repackaged.com.google.gson.Gson;
+import com.google.appengine.repackaged.com.google.gson.GsonBuilder;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoDatabase;
 import edu.csula.datascience.acquisition.Collector;
 import edu.csula.datascience.r.models.Comment;
 import edu.csula.datascience.r.models.Post;
 import net.dean.jraw.models.Submission;
+import org.bson.Document;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 
 /**
  * Created by tj on 4/21/16.
  */
 public class SubmissionCollector implements Collector<Post, Submission> {
+  private final String USERNAME = "MONGO_USERNAME";
+  private final String PASSWORD = "MONGO_PASSWORD";
+  private final String HOST = "MONGO_HOST";
+  private final String DB_NAME = "MONGO_DB_NAME";
+
+  private MongoClient mongoClient;
+  private MongoDatabase db;
+
   @Override
   public Collection<Post> mungee(Collection<Submission> src) {
     List<Post> posts = new ArrayList<>(src.size());
@@ -56,6 +68,60 @@ public class SubmissionCollector implements Collector<Post, Submission> {
   @Override
   public void save(Collection<Post> data) {
     System.out.println("saving data");
+    String dbName = connectDatabase();
+    List<Document> docs = new ArrayList<>();
+    for(Post post : data){
+      docs.add(createDocument(post));
+    }
+    insertMany(docs, dbName);
+    closeDatabase();
+  }
+
+  private String connectDatabase(){
+    Map<String, String> envMap = readDatabaseCredentials();
+    String dbUser = envMap.get(USERNAME);
+    String dbPass = envMap.get(PASSWORD);
+    String host = envMap.get(HOST);
+    String dbName = envMap.get(DB_NAME);
+    MongoClientURI uri = new MongoClientURI(
+            "mongodb://" + dbUser + ":" + dbPass + "@" + host + "/?authSource=" + dbName);
+    mongoClient = new MongoClient(uri);
+    db = mongoClient.getDatabase(dbName);
+    return dbName;
+  }
+
+  private void closeDatabase(){
+    mongoClient.close();
+  }
+
+  private Map<String, String> readDatabaseCredentials(){
+    Map<String, String> envMap = new HashMap<>(4);
+    if(System.getenv(USERNAME) == null){
+      System.err.println("Could not find environment variables. Did you source the environment?");
+      System.err.println("source <filename>.env");
+      System.exit(1);
+    }
+    envMap.put(USERNAME, System.getenv(USERNAME));
+    envMap.put(PASSWORD, System.getenv(PASSWORD));
+    envMap.put(HOST, System.getenv(HOST));
+    envMap.put(DB_NAME, System.getenv(DB_NAME));
+    return envMap;
+  }
+
+  private Document createDocument(Post post){
+    GsonBuilder builder = new GsonBuilder();
+    Gson gson = builder.create();
+    String json = gson.toJson(post);
+    Document doc = Document.parse(json);
+    return doc;
+  }
+
+  private void insertDocument(Document doc, String dbName){
+    db.getCollection(dbName).insertOne(doc);
+  }
+
+  private void insertMany(List<Document> documents, String dbName){
+    db.getCollection(dbName).insertMany(documents);
   }
 
 }
